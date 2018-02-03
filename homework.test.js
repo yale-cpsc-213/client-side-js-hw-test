68f743ecd6126a832a336f2a1ca36f2968f0fdd7
Kyle L. Jensen <kljensen@gmail.com>
2018-02-02 19:33:52 -0500

    Test for URL env variable

---
+++ homework.test.js
@@ -0,0 +1,37 @@
+/* The following comment line helps ESlint know which
+   variables are global in scope.
+*/
+
+/* global beforeAll, afterAll, test, expect */
+const puppeteer = require('puppeteer');
+const validator = require('validator');
+
+let browser;
+let page;
+let url;
+
+/*  This functions run before all tests and
+    it initializes the headless chrome browser
+    that we're using to perform the tests.
+*/
+beforeAll(async () => {
+    browser = await puppeteer.launch();
+    page = await browser.newPage();
+});
+
+/* This function runs after all the tests are
+   done and it closes the browser we were using.
+*/
+afterAll(async () => {
+    await browser.close();
+});
+
+test('The URL environment variable is defined and is a valid URL', () => {
+    url = process.env.URL || '';
+    const options = {
+        require_protocol: true,
+        protocols: ['http', 'https'],
+        require_tld: false,
+    };
+    expect(validator.isURL(url, options)).toBe(true);
+});
